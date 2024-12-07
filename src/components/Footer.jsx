062dbaa1f5eaa4b2229c3f799e1f7428276229ae
syedmehedi34 <syedmeehdi34@gmail.com>
2024-12-07 22:39:05 +0600

    sorting by genre done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,27 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer bg-base-200 text-base-content p-10 dark:bg-dark border-t border-t-white/15">
-      <nav className="dark:text-white">
+      <nav className="dark:text-white/90">
         <h6 className="footer-title">Services</h6>
         <p className="link link-hover">Our Services</p>
         <p className="link link-hover">Designer</p>
         <p className="link link-hover">Our Games</p>
         <p className="link link-hover">Advertisement</p>
       </nav>
-      <nav className="dark:text-white">
+      <nav className="dark:text-white/90">
         <h6 className="footer-title">Company</h6>
         <p className="link link-hover">About us</p>
         <p className="link link-hover">Contact</p>
         <p className="link link-hover">Web Portal</p>
         <p className="link link-hover">Developers</p>
       </nav>
-      <nav className="dark:text-white">
+      <nav className="dark:text-white/90">
         <h6 className="footer-title">Legal</h6>
         <p className="link link-hover">Terms of use</p>
         <p className="link link-hover">Privacy policy</p>
         <p className="link link-hover">Cookie policy</p>
       </nav>
-      <div className="dark:text-white">
+      <div className="dark:text-white/90">
         <h6 className="footer-title">Newsletter</h6>
         <fieldset className="form-control w-80">
           <label className="label">
--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -4,10 +4,11 @@ import ReviewCards from "../components/ReviewCards";
 
 const AllReviews = () => {
   const data = useLoaderData();
+  const [games, setGames] = useState(data);
   const [reviews, setReviews] = useState(data);
 
-  // ? Sorting Options ----------------
-  const handleSelectChange = (event) => {
+  // * Sorting Options ----------------
+  const handleSortBy = (event) => {
     const value = event.target.value;
 
     if (value === "ratings") {
@@ -23,24 +24,55 @@ const AllReviews = () => {
     }
   };
 
-  // console.log(reviews);
+  // * sort by genre
+  const genres = [...new Set(data.map((p) => p.genre))];
+
+  const handleGenreChange = (event) => {
+    setReviews(data);
+    const genre = event.target.value;
+    console.log(genre);
+
+    const filteredGames =
+      genre === "All"
+        ? games
+        : games.filter((review) => review.genre === genre);
+    setReviews(filteredGames);
+    console.log(filteredGames);
+  };
 
   //--------------------------------------
   return (
     <div className="w-11/12 mx-auto mt-28">
       {/* Sorting Starts */}
       <div className="py-4">
-        <div className="flex justify-center ">
-          <select
-            className="select select-bordered w-full max-w-xs"
-            onChange={handleSelectChange}
-          >
-            <option disabled selected>
-              Who shot first?
-            </option>
-            <option value="ratings">Short By Ratings</option>
-            <option value="year">Short By Year</option>
-          </select>
+        <div className="flex justify-between ">
+          <div>
+            <select
+              className="select select-bordered w-full max-w-xs"
+              onChange={handleSortBy}
+            >
+              <option disabled selected>
+                Who shot first?
+              </option>
+              <option value="ratings">Short By Ratings</option>
+              <option value="year">Short By Year</option>
+            </select>
+          </div>
+
+          <div>
+            <select
+              onChange={handleGenreChange}
+              className="select select-bordered w-full max-w-xs"
+              // value={selectedGenre}
+            >
+              <option value="All">All</option>
+              {genres.map((genre, index) => (
+                <option key={index} value={genre}>
+                  {genre}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
 
